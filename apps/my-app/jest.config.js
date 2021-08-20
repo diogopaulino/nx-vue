c36e6f27b9944535947660bdaf371189a5758991
Diogo Paulino <diogopaulino.web@gmail.com>
2021-08-20 19:33:14 -0300

    chore: init project

---
+++ apps/my-app/jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  displayName: 'my-app',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'vue', 'js', 'json'],
+  coverageDirectory: '../../coverage/apps/my-app',
+  snapshotSerializers: ['jest-serializer-vue'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+    'vue-jest': {
+      tsConfig: `${__dirname}/tsconfig.spec.json`,
+    },
+  },
+};
